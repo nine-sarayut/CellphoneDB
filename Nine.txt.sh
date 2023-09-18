8e089cba050e6d896d45b35719afe3ad08e2fb25
Sarayut "Nine" Winuthayanon <110446234+nine-sarayut@users.noreply.github.com>
2023-09-17 21:18:01 -0500

    Update Nine.txt.sh

--- Nine.txt.sh
+++ Nine.txt.sh
@@ -1,68 +1,87 @@
 # wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+mkdir -p  ~/Documents/Github
+cd  ~/Documents/Github
+git clone https://github.com/ventolab/CellphoneDB
+cd  ~/Documents/Github/CellphoneDB
+# mkdir -p ~/conda-envs/cpdb3.8
+# conda create -p ~/conda-envs/cpdb3.8 python=3.8 -y
+# conda activate /Users/sw7v6/conda-envs/cpdb3.8
+conda create -n cpdb3.8 python=3.8 -y
+conda activate cpdb3.8
+pip install -e .
+pip install ipykernel
+python3 -m ipykernel install --user --name 'cpdb3.8'
+pip install scanpy ktplotspy==0.1.10
+# pip install cellphonedb
+# duplicate *.ipynb
+# edit T00_DownloadDB-Copy1.ipynb and change kernel to cpdb3.8
+cpdb_target_dir = os.path.join('/Users/sw7v6/Documents/Github/CellphoneDB/cpdb_tutorial/db', cpdb_version)
+
+mkdir -p ~/Documents/Github/CellphoneDB/cpdb_tutorial/data
+cd ~/Documents/Github/CellphoneDB/cpdb_tutorial/data
+unzip ~/Documents/Github/CellphoneDB/notebooks/data_tutorial.zip
+
+# edit T00_BuildDBfromFiles-Copy1.ipynb
+cpdb_input_dir = '/Users/sw7v6/git/CellphoneDB/cpdb_tutorial/db/v4.1.0/' 
+
+# edit T01_Method1-Copy1.ipynb
+os.chdir('/Users/sw7v6/Documents/Github/CellphoneDB/cpdb_tutorial')
+
+mkdir -p  ~/Documents/Github
+cd  ~/Documents/Github
+git clone https://github.com/zktuong/ktplotspy
+cd  ~/Documents/Github/ktplotspy
+conda create -n cpdb3.9 python=3.9 -y
+conda activate cpdb3.9
+pip install -e .
+pip install ipykernel
+python3 -m ipykernel install --user --name 'cpdb3.9'
+pip install cellphonedb scanpy
+
 # https://github.com/Teichlab/cellphonedb
 conda create -n cpdb3.7 python=3.7 -y
 conda activate cpdb3.7
 pip3 install cellphonedb==2.1.7
 cellphonedb --help
+mkdir -p  ~/Documents/Github/cellphonedb2
+cd ~/Documents/Github/cellphonedb2
 curl https://raw.githubusercontent.com/Teichlab/cellphonedb/master/in/example_data/test_counts.txt --output test_counts.txt
 curl https://raw.githubusercontent.com/Teichlab/cellphonedb/master/in/example_data/test_meta.txt --output test_meta.txt
 cellphonedb method statistical_analysis test_meta.txt test_counts.txt
 cellphonedb plot dot_plot
 cellphonedb plot heatmap_plot test_meta.txt
 
-mkdir -p ~/git
-cd ~/git
-git clone git@github.com:ventolab/CellphoneDB.git
-cd CellphoneDB
-pip install -e .
-# conda create -n cpdb python=3.9.13 -y
-# conda create -n cpdb python=3.9 -y
-conda create -n cpdb python=3.8 -y
-conda activate cpdb
-# pip install -r requirements.txt
-pip install \
-pandas \
-numpy \
-numpy-groupies \
-requests \
-scikit-learn \
-geosketch \
-anndata \
-ktplotspy \
-tqdm \
-scanpy \
-jupyterlab \
-ipykernel \
-cython \
-cellphonedb
-
-pip install ktplotspy==0.1.10
-pip install cellphonedb
-# jupyter lab build
-python -m ipykernel install --user --name 'cpdb'
-jupyter-lab --no-browser --ip 0.0.0.0
-# jupyter-lab
-
-# edit T00_DownloadDB.ipynb
-cpdb_target_dir = os.path.join('/Users/sw7v6/git/CellphoneDB/cpdb_tutorial/db', cpdb_version)
-# edit T00_BuildDBfromFiles.ipynb
-cpdb_input_dir = '/Users/sw7v6/git/CellphoneDB/cpdb_tutorial/db/v4.1.0/' 
-
-mkdir -p ~/git/CellphoneDB/cpdb_tutorial/data
-cd ~/git/CellphoneDB/cpdb_tutorial/data
-unzip ~/git/CellphoneDB/notebooks/data_tutorial.zip
-
-# edit T01_Method1.ipynb
-os.chdir('/Users/sw7v6/git/CellphoneDB/cpdb_tutorial')
-# and 
-cpdb_file_path = 'db/v4.1.0/cellphonedb_09_14_2023_210958.zip'
-
 #***** remove-start *****#
 # jupyter kernelspec list
-jupyter kernelspec remove -f cpdb
+jupyter kernelspec remove -f cpdb3.8
+jupyter kernelspec remove -f cpdb3.9
+jupyter kernelspec remove -f cpdb3.10
+jupyter kernelspec remove -f cpdb3.11
 
 conda deactivate
 
 # conda env list
 conda remove --name cpdb --all -y
 #***** remove-end *****#
+
+# conda create -n cpdb python=3.8 -y
+# conda activate cpdb
+# pip install -r requirements.txt
+# pip install \
+# pandas \
+# numpy \
+# numpy-groupies \
+# requests \
+# scikit-learn \
+# geosketch \
+# anndata \
+# ktplotspy \
+# tqdm \
+# scanpy \
+# jupyterlab \
+# ipykernel \
+# cython \
+# cellphonedb
+
+# jupyter-lab --no-browser --ip 0.0.0.0
+# jupyter-lab
